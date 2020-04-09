7fdb685bc887ac1c7a1db786c87b40769a25323a
Adam G Horn <hornadam8@gmail.com>
2020-04-09 12:17:22 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,4 +7,16 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     self.order(rating: :desc).first
   end
+  
+  def self.lowest_rating
+    self.minimum("rating")
+  end
+  
+  def self.least_popular_show
+    self.order(rating: :asc).first
+  end
+  
+  def self.ratings_sum
+    self.sum("rating")
+  end
 end
